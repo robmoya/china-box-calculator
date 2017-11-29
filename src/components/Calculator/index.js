0f647ec44f3b8c8c6a0b56add4e08f8c6756ef7f
Rob Moya <robmoya@benzinga.com>
2017-11-28 23:47:16 -0600

    initial commit

---
+++ src/components/Calculator/index.js
@@ -0,0 +1,11 @@
+import React , { Component } from 'react';
+
+class Calculator extends Component {
+    render(){
+        return(
+            <h1>Calculator</h1>
+        )
+    }
+}
+
+export default Calculator
